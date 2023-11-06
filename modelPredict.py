bb67968bcfc32da204e6648ad9dc9da93d84b9ed
lekshman-babu <118737588+lekshman-babu@users.noreply.github.com>
2023-11-06 17:53:28 +0530

    Create modelPredict.py

---
+++ modelPredict.py
@@ -0,0 +1,12 @@
+import joblib
+import preProcessing as pp
+import numpy as np
+import keyVariables as kv
+
+model=joblib.load("movelytics84.75.pkl")
+def predict(path):
+    predictVideo=pp.preprocessor(path)
+    allLabels=model.predict(np.expand_dims(predictVideo,axis=0))[0]
+    predictedIndex=np.argmax(allLabels)
+    predictedClass=kv.CLASS_LABLES[predictedIndex]
+    return predictedClass
